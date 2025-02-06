95582700fa2fd59acd916f43a7e40b07e34e9cec
Daniel Camargo <daniel.camargodelima2@gmail.com>
2025-02-06 11:58:08 -0300

    Separando os filmes com php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-<?php include './includes/header.php' ?>
+<?php
+$titulo = 'CineBox - Inicio';
+include './includes/header.php' ?>
 
 <section id="banner">
     <main class="carrossel-container">
@@ -19,12 +21,14 @@
     <h2 class="titulo">Filmes</h2>
     <main class="container">
         <div class="row">
+
+        <?php for ($i = 0; $i < 8; $i++) { ?>
             <div class="col col-3">
                 <figure>
                     <img src="./assets/img/poster/avatar-o-caminho-da-agua.png"
                         alt="poster do filme avatar o caminho da água" class="foto-produto">
                     <figcaption>
-                        <h4>Avatar 2- O Caminho da Água</h4>
+                        <h4>Avatar o Caminho da Água</h4>
                         <span class="preco">R$ 15,90</span>
                         <p class="descricao">Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi eu
                             sit. Vide electram sadipscing et per. Per aumento de cachacis, eu reclamis. Paisis, filhis,
@@ -37,138 +41,7 @@
                     </span>
                 </figure>
             </div>
-
-            <div class="col col-3">
-                <figure>
-                    <img src="./assets/img/poster/gente-grande-2.png" alt="poster do filme gente grande 2"
-                        class="foto-produto">
-                    <figcaption>
-                        <h4>Gente Grande 2</h4>
-                        <span class="preco">R$ 120,00</span>
-                        <p class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-                            assumenda ipsam corporis? Optio labore, facere possimus ea hic laudantium magni saepe
-                            aut, praesentium ducimus doloremque in soluta ex ipsa suscipit.</p>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #D966BA;">Ficção</label>
-                    </span>
-                </figure>
-            </div>
-
-            <div class="col col-3">
-                <figure>
-                    <img src="./assets/img/poster/bad-boys-ate-o-fim.png" alt="poster do filme bad boys até o fim"
-                        class="foto-produto">
-                    <figcaption>
-                        <h4>Bad Boys Até o Fim</h4>
-                        <span class="preco">R$ 15,90</span>
-                        <p class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-                            assumenda ipsam corporis? Optio labore, facere possimus ea hic laudantium magni saepe
-                            aut, praesentium ducimus doloremque in soluta ex ipsa suscipit.</p>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #D966BA;">Ficção</label>
-                    </span>
-                </figure>
-            </div>
-
-            <div class="col col-3">
-                <figure>
-                    <img src="./assets/img/poster/coringa-2.png" alt="poster do filme coringa 2" class="foto-produto">
-                    <figcaption>
-                        <h4>Coringa 2</h4>
-                        <span class="preco">R$ 15,90</span>
-                        <p class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-                            assumenda ipsam corporis? Optio labore, facere possimus ea hic laudantium magni saepe
-                            aut, praesentium ducimus doloremque in soluta ex ipsa suscipit.</p>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #D966BA;">Ficção</label>
-                    </span>
-                </figure>
-            </div>
-
-            <div class="col col-3">
-                <figure>
-                    <img src="./assets/img/poster/pulp-fiction.png" alt="poster do filme pulp fiction"
-                        class="foto-produto">
-                    <figcaption>
-                        <h4>Pulp Fiction</h4>
-                        <span class="preco">R$ 15,90</span>
-                        <p class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-                            assumenda ipsam corporis? Optio labore, facere possimus ea hic laudantium magni saepe
-                            aut, praesentium ducimus doloremque in soluta ex ipsa suscipit.</p>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #D966BA;">Ficção</label>
-                    </span>
-                </figure>
-            </div>
-
-            <div class="col col-3">
-                <figure>
-                    <img src="./assets/img/poster/sem-tempo-para-morrer.png" alt="poster do filme sem tempo para morrer"
-                        class="foto-produto">
-                    <figcaption>
-                        <h4>Sem Tempo Para Morrer</h4>
-                        <span class="preco">R$ 15,90</span>
-                        <p class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-                            assumenda ipsam corporis? Optio labore, facere possimus ea hic laudantium magni saepe
-                            aut, praesentium ducimus doloremque in soluta ex ipsa suscipit.</p>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #D966BA;">Ficção</label>
-                    </span>
-                </figure>
-            </div>
-
-            <div class="col col-3">
-                <figure>
-                    <img src="./assets/img/poster/star-wars-the-last-jedi.png"
-                        alt="poster do filme star wars the last Jedi" class="foto-produto">
-                    <figcaption>
-                        <h4>Star Wars The Last Jedi</h4>
-                        <span class="preco">R$ 15,90</span>
-                        <p class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-                            assumenda ipsam corporis? Optio labore, facere possimus ea hic laudantium magni saepe
-                            aut, praesentium ducimus doloremque in soluta ex ipsa suscipit.</p>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #D966BA;">Ficção</label>
-                    </span>
-                </figure>
-            </div>
-
-            <div class="col col-3">
-                <figure>
-                    <img src="./assets/img/poster/the-batman.png" alt="poster do filme the batman" class="foto-produto">
-                    <figcaption>
-                        <h4>The Batman</h4>
-                        <span class="preco">R$ 15,90</span>
-                        <p class="descricao">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-                            assumenda ipsam corporis? Optio labore, facere possimus ea hic laudantium magni saepe
-                            aut, praesentium ducimus doloremque in soluta ex ipsa suscipit.</p>
-                    </figcaption>
-                    <span class="genero">
-                        <label style="background-color: #070373;">Fantasia</label>
-                        <label style="background-color: #580259;">Drama</label>
-                        <label style="background-color: #D966BA;">Ficção</label>
-                    </span>
-                </figure>
-            </div>
-
+            <?php } ?>
 
         </div>
     </main>
